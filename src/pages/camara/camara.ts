442a931abc0c3070cfff78b03bb4e46575d4a253
Lucho <hpcarg@gmail.com>
2019-04-04 06:38:02 -0300

    galeria

--- src/pages/camara/camara.ts
+++ src/pages/camara/camara.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
 
@@ -8,14 +8,17 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
   selector: 'page-camara',
   templateUrl: 'camara.html',
 })
-export class CamaraPage {
+export class CamaraPage
+{
 
-  image: string = null;
+  contenidoFoto;
+
 
   constructor(
-    private camera: Camera
+    private camera: Camera,
+    private toastCtrl: ToastController
   ) {}
-
+  //Usa la camara para sacar una foto
   tomarFoto()
   {
     const options: CameraOptions =
@@ -23,16 +26,53 @@ export class CamaraPage {
       quality: 100,
       destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: true,
+      correctOrientation: true,
+      saveToPhotoAlbum: true
     }
 
-    this.camera.getPicture(options).then((imageData) => {
-    // imageData is either a base64 encoded string or a file URI
-    // If it's base64 (DATA_URL):
-    let base64Image = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
+    this.camera.getPicture(options).then((imageData) =>
+    {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.contenidoFoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) =>
+    {
     // Handle error
+      this.mostrarToast("Hubo un error con la camara");
     });
 
+  }
+
+  //Para obtener una foto de la galeria
+  obtenerFoto(){
+    const options: CameraOptions =
+    {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+
+    this.camera.getPicture(options).then((imageData) =>
+    {
+      this.contenidoFoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) =>
+    {
+    // Handle error
+      this.mostrarToast("No se ha seleccionado la imagen");
+    });
+  }
+
+  mostrarToast(msg)
+  {
+    let toast = this.toastCtrl.create
+    ({
+      message: msg,
+      duration: 3000,
+      position: "bottom"
+    })
+    toast.present();
   }
 }
